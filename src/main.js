0e94d9463b01ed2cfce90734828e696629d04d66
kovlento <bluskytee@163.com>
2021-05-30 01:22:08 +0800

    recommendlist

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import lazyPlugin from 'vue3-lazy'
 
 // 引入全局样式文件
 import '@/assets/scss/index.scss'
@@ -9,4 +10,7 @@ import '@/assets/scss/index.scss'
 createApp(App)
   .use(store)
   .use(router)
+  .use(lazyPlugin, {
+    loading: require('@/assets/images/default.png')
+  })
   .mount('#app')
